83d4539aecd8c606e46f55c156228f5640859b16
adityanair42 <adityamnair42@gmail.com>
2025-07-21 06:34:11 +0000

    finished http chat return logic

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -101,6 +101,32 @@ app.post('/room', middleware, async (req, res) => {
   }
 })
 
+app.get("/chats/:roomId", async (req, res) => {
+    try {
+        const roomId = Number(req.params.roomId);
+        console.log(req.params.roomId);
+        const messages = await prismaClient.chat.findMany({
+            where: {
+                roomId: roomId
+            },
+            orderBy: {
+                id: "desc"
+            },
+            take: 50
+        });
+
+        res.json({
+            messages
+        })
+    } catch(e) {
+        console.log(e);
+        res.json({
+            messages: []
+        })
+    }
+    
+})
+
 app.get("/room/:slug", async (req, res) => {
     const slug = req.params.slug;
     const room = await prismaClient.room.findFirst({
