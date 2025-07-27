b3ac97163bd87a4d5b75d880a7db3719bf8e29a9
adityanair42 <adityamnair42@gmail.com>
2025-07-27 10:40:05 +0000

    finished signup and login fe

--- apps/frontend/app/signin/page.tsx
+++ apps/frontend/app/signin/page.tsx
@@ -1,33 +1,19 @@
-export default function Signin() {
-    return <div className="h-screen flex justify-center flex-col">
-        <div className="flex justify-center">
-        <a href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 ">
-                <div>
-                    <div className="px-10">
-                        <div className="text-3xl font-extrabold">
-                            Sign in
-                        </div>
-                    </div>
-                    <div className="pt-2">
-                        <LabelledInput label="Username" placeholder="harkirat@gmail.com" />
-                        <LabelledInput label="Password" type={"password"} placeholder="123456" />
-                        <button type="button" className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Sign in</button>
-                    </div>
-                </div>
-            </a>
+export default function SignIn() {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="relative flex justify-center items-center h-111 w-71 rounded-xl bg-white bg-gradient-to-r from-orange-500 to-red-800">
+        <div className="flex flex-col items-center rounded-xl h-110 w-70 bg-black">
+        <div className="text-3xl text-neutral-300 mt-13 mb-17 font-medium">
+          Sign In
         </div>
-    </div>
-}
+        <input type="text" placeholder="email"        className="py-2 border rounded-md px-2 my-2 bg-neutral-800"/>
+        <input type="password" placeholder="password" className="py-2 border rounded-md px-2 my-2 bg-neutral-800" />
 
-interface LabelledInputType {
-    label: string;
-    placeholder: string;
-    type?: string;
-}
-
-function LabelledInput({ label, placeholder, type }: LabelledInputType) {
-    return <div>
-        <label className="block mb-2 text-sm text-black font-semibold pt-4">{label}</label>
-        <input type={type || "text"} id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder={placeholder} required />
+        <button className="mt-17 px-21 rounded-md py-2 bg-gradient-to-r from-orange-500 to-red-800">
+          Sign In
+        </button>
+      </div>
+      </div>
     </div>
+  ) 
 }
--- apps/frontend/app/signup/page.tsx
+++ apps/frontend/app/signup/page.tsx
@@ -1,13 +1,19 @@
 export default function SignIn() {
   return (
     <div className="flex justify-center items-center min-h-screen">
-      <div className="flex flex-col items-center border-2 rounded-xl h-100 w-70 ">
-        <div className="text-3xl mt-15 mb-15">
+      <div className="relative flex justify-center items-center h-111 w-71 rounded-xl bg-white bg-gradient-to-r from-orange-500 to-red-800">
+        <div className="flex flex-col items-center rounded-xl h-110 w-70 bg-black">
+        <div className="text-3xl text-neutral-300 mt-13 mb-10 font-medium">
           Sign Up
         </div>
-        <input type="text" placeholder="email" />
-        <input type="text" placeholder="name"/>
-        <input type="password" placeholder="password"/>
+        <input type="text" placeholder="email"        className="py-2 border rounded-md px-2 my-2 bg-neutral-800"/>
+        <input type="text" placeholder="name"         className="py-2 border rounded-md px-2 my-2 bg-neutral-800" />
+        <input type="password" placeholder="password" className="py-2 border rounded-md px-2 my-2 bg-neutral-800" />
+
+        <button className="mt-10 px-20 rounded-md py-2 bg-gradient-to-r from-orange-500 to-red-800">
+          Sign Up
+        </button>
+      </div>
       </div>
     </div>
   )
