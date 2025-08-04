1f1816c8eff2bc251607cdd0cfd07b165f145626
adityanair42 <adityamnair42@gmail.com>
2025-08-04 14:27:49 +0000

    added dynamic input in landing page

--- apps/frontend/components/HeroSection.tsx
+++ apps/frontend/components/HeroSection.tsx
@@ -1,7 +1,14 @@
+"use client"
+import { useState } from "react";
+
 export function HeroSection() {
-  return(
+
+  const [Input1, setInput1] = useState(false);
+  const [Input2, setInput2] = useState(false);
+
+  return (
     <div className="flex flex-col items-center mt-45">
-      <h1  className="text-7xl text-center tracking-wide bg-gradient-to-r from-neutral-400 to-neutral-200 bg-clip-text text-transparent pb-2">
+      <h1 className="text-7xl text-center tracking-wide bg-gradient-to-r from-neutral-400 to-neutral-200 bg-clip-text text-transparent pb-2">
         Messenger
         <span className="bg-gradient-to-r from-orange-500 to-red-800 text-transparent bg-clip-text">
           {" "}V2
@@ -10,13 +17,35 @@ export function HeroSection() {
       <p className="mt-10 text-lg text-center text-neutral-500 max-w-4xl">
         Secure, instant and reliable communication. <br /> Our scalable platform uses websockets and robust encryption to empower your connections.
       </p>
-      <div className="mt-15">
-        <button className="py-3 px-4 mx-3 rounded-md border">
-          Create a room
-        </button>
-        <button className="bg-gradient-to-r from-orange-500 to-orange-800 py-3 px-4 mx-3 rounded-md">
-          Join a room
-        </button>
+      <div className="mt-15 flex justify-center">
+        {!Input1 && (
+          <div>
+            <button className="py-3 px-4 mx-3 rounded-md border" onClick={() => {
+              setInput1(!Input1)
+            }}>
+              Create a room
+            </button>
+          </div>
+        )}
+        {Input1 && (
+          <div>
+            <input type="text" placeholder="Enter room number" className="py-3 px-4 mx-3 rounded-md border" />
+          </div>
+        )}
+        {!Input2 && (
+          <div>
+            <button className="bg-gradient-to-r from-orange-500 to-orange-800 py-3 px-4 mx-3 rounded-md" onClick={() => {
+              setInput2(!Input2)
+            }}>
+              Join a room
+            </button>
+          </div>
+        )}
+        {Input2 && (
+          <div>
+            <input type="text" placeholder="Enter room number" className="bg-gradient-to-r from-orange-500 to-orange-800 py-3 px-4 mx-3 rounded-md" />
+          </div>
+        )}
       </div>
     </div>
   )
