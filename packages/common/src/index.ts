8dfe055583a525c0ca5777f4f069e186c273b06a
adityanair42 <adityamnair42@gmail.com>
2025-08-06 05:35:30 +0000

    prep for vercel deployment

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,4 +1,3 @@
-
-export const JWT_SECRET = process.env.JWT_SECRET || "123123";
-export const BACKEND_URL = "http://localhost:3001"
-export const WS_URL = "ws://localhost:8080"
+export const JWT_SECRET = process.env.JWT_SECRET || "default_secret";
+export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:3001";
+export const WS_URL = process.env.NEXT_PUBLIC_WS_URL || "ws://localhost:8080";
